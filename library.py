04a484c2a6c529c8d6c8a762e446c7ee5f53e36c
luventiusbravengit config --global user.name luventiusbraven <luventiusbraven@gmail.com>
2026-05-28 14:12:30 +0800

    finished library.py

--- library.py
+++ library.py
@@ -1,8 +1,12 @@
 import random
 
 def gen_random_int(number: int, seed: int) -> list[int]:
-    pass
     ### your code ###
+    result = None
+    random.seed(seed)
+    result = range(0, number)
+    result = list(result)
+    random.shuffle(result)
 
 
 def create_string(array: list[int]) -> str:
@@ -21,4 +25,14 @@ def create_string(array: list[int]) -> str:
 
 def my_sort(array):
     ### your code ###
-    pass
+    # insertion sort
+
+    n = len(array)
+    for outer_index in range(1, n):
+        inner_index = outer_index
+        while inner_index > 0 and array[inner_index] < array[inner_index - 1]:
+            first_number = array[inner_index - 1]
+            second_number = array[inner_index]
+            array[inner_index - 1] = second_number
+            array[inner_index] = first_number
+            inner_index -= 1
