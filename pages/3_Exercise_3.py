a6478532eb2f2440fcf801bd85261673347751f5
marcusim19chen-ui <marcusim.19chen@gmail.com>
2026-06-05 09:28:06 +0800

    Update 3_Exercise_3.py

--- pages/3_Exercise_3.py
+++ pages/3_Exercise_3.py
@@ -1,1 +1,1 @@
-# Odd-Even Sort (Brick Sort) - in-place, stable sorting algorithm, time complexity is ok Worst: O(n²) Best: O(n) (already sorted)
+# Cycle Sort - in-place, stable sorting algorithm, time complexity is ok Worst: O(n²) Best: O(n) (already sorted)
